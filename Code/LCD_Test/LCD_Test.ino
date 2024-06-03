14db0927de37c668cb2c645640818f3bed13d034
Asif Ahmed <inbox.asifahmed@gmail.com>
2024-06-03 19:22:56 +0200

    .

--- Code/LCD_Test/LCD_Test.ino
+++ Code/LCD_Test/LCD_Test.ino
@@ -1,24 +1,19 @@
-#include <Wire.h>                      
-#include <LiquidCrystal_I2C.h> //https://github.com/marcoschwartz/LiquidCrystal_I2C/archive/master.zip  
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x3F, 20, 4);
 
-LiquidCrystal_I2C lcd(0x3F,20,4);     
-
-void setup()
-
-{
+void setup() {
   lcd.init();
-  lcd.backlight();                
-  lcd.setCursor(0,0);           
+  lcd.backlight();
+  lcd.setCursor(0, 0);
   lcd.print("Test Line 1");
-  lcd.setCursor(1,0);             
+  lcd.setCursor(1, 0);
   lcd.print("Test Line 2");
-  lcd.setCursor(2,0);            
+  lcd.setCursor(2, 0);
   lcd.print("Test Line 3");
-  lcd.setCursor(3,0);             
+  lcd.setCursor(3, 0);
   lcd.print("Test Line 4");
 }
 
-void loop()
-{
-
+void loop() {
 }
